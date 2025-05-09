607fe26750aa969bee561e42455ee3e297972adf
fransfilastap@live.com <fransfilastap@Live.com>
2025-05-09 11:09:29 +0700

    disable sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,51 +1,53 @@
 // @ts-check
-import {defineConfig} from 'astro/config';
+import {defineConfig, passthroughImageService} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'ILDIS',
-          social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/bphndigitalservice/ildis'}],
-          tableOfContents: {minHeadingLevel: 2, maxHeadingLevel: 2},
-          defaultLocale: 'id',
-          locales: {
-              root: {
-                  label: 'Bahasa Indonesia',
-                  lang: 'id', // lang is required for root locales
-              },
+  integrations: [starlight({
+      title: 'ILDIS',
+      social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/bphndigitalservice/ildis'}],
+      tableOfContents: {minHeadingLevel: 2, maxHeadingLevel: 2},
+      defaultLocale: 'id',
+      locales: {
+          root: {
+              label: 'Bahasa Indonesia',
+              lang: 'id', // lang is required for root locales
           },
-          sidebar: [
-              {
-                  label: 'Pendahuluan',
-                  items: [
-                      {label: 'Tentang ILDIS', link: '/tentang-ildis'},
-                      {label: 'Persyaratan Sistem', link: '/persyaratan-sistem'},
-                  ],
-              },
-              {
-                  label: 'Instalasi & Konfigurasi',
-                  items: [
-                      {label: 'Instalasi', link: '/instalasi'},
-                      {label: 'Konfigurasi', link: '/konfigurasi'},
-                      {label: 'Struktur Direktori', link: '/struktur-direktori'},
-                      {label: 'Deployment', link: '/deployment'},
-                  ]
-              },
-              {
-                  label: 'Troubleshooting',
-                  autogenerate: {directory: '/troubleshooting/', collapsed: true},
-              },
-              {
-                  label: 'Kontribusi',
-                  link: '/kontribusi',
-              }
-          ],
-      }),
-  ],
-
+      },
+      sidebar: [
+          {
+              label: 'Pendahuluan',
+              items: [
+                  {label: 'Tentang ILDIS', link: '/tentang-ildis'},
+                  {label: 'Persyaratan Sistem', link: '/persyaratan-sistem'},
+              ],
+          },
+          {
+              label: 'Instalasi & Konfigurasi',
+              items: [
+                  {label: 'Instalasi', link: '/instalasi'},
+                  {label: 'Konfigurasi', link: '/konfigurasi'},
+                  {label: 'Struktur Direktori', link: '/struktur-direktori'},
+                  {label: 'Deployment', link: '/deployment'},
+              ]
+          },
+          {
+              label: 'Troubleshooting',
+              autogenerate: {directory: '/troubleshooting/', collapsed: true},
+          },
+          {
+              label: 'Kontribusi',
+              link: '/kontribusi',
+          }
+      ],
+  }), sitemap()],
+    image: {
+        service: passthroughImageService(),
+    },
   adapter: vercel(),
 });
