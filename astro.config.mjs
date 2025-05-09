c35e4dd4a47eb4132e799543a63f2f6c245cbc33
fransfilastap@live.com <fransfilastap@Live.com>
2025-05-09 10:58:09 +0700

    initialize documentation structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+      starlight({
+          title: 'ILDIS',
+          social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/bphndigitalservice/ildis'}],
+          tableOfContents: {minHeadingLevel: 2, maxHeadingLevel: 2},
+          defaultLocale: 'id',
+          locales: {
+              root: {
+                  label: 'Bahasa Indonesia',
+                  lang: 'id', // lang is required for root locales
+              },
+          },
+          sidebar: [
+              {
+                  label: 'Pendahuluan',
+                  items: [
+                      {label: 'Tentang ILDIS', link: '/tentang-ildis'},
+                      {label: 'Persyaratan Sistem', link: '/persyaratan-sistem'},
+                  ],
+              },
+              {
+                  label: 'Instalasi & Konfigurasi',
+                  items: [
+                      {label: 'Instalasi', link: '/instalasi'},
+                      {label: 'Konfigurasi', link: '/konfigurasi'},
+                      {label: 'Struktur Direktori', link: '/struktur-direktori'},
+                      {label: 'Deployment', link: '/deployment'},
+                  ]
+              },
+              {
+                  label: 'Troubleshooting',
+                  autogenerate: {directory: '/troubleshooting/', collapsed: true},
+              },
+              {
+                  label: 'Kontribusi',
+                  link: '/kontribusi',
+              }
+          ],
+      }),
+  ],
+
+  adapter: vercel(),
 });
